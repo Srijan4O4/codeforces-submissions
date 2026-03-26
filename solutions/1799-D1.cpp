7bdb5d72ac2b273c42c0599cfce4b6627f0d6587
Srijan Agrawal <srijanagrawal89@gmail.com>
2026-03-26 14:43:03 +0530

    Add Hot Start Up (easy version) (1799-D1)

---
+++ solutions/1799-D1.cpp
@@ -0,0 +1,10 @@
+// Hot Start Up (easy version)
+// Contest: 1799
+// Problem: D1
+// Submission ID: 367911967
+// URL: https://codeforces.com/contest/1799/submission/367911967
+
+// Language: C++23 (GCC 14-64, msys2)
+// Verdict: OK
+
+// TODO: Replace with actual solution code
