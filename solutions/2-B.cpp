12f1d639dd68d071223b42e31e5aee57a1602252
Srijan Agrawal <srijanagrawal89@gmail.com>
2026-03-26 14:43:02 +0530

    Add The least round way (2-B)

---
+++ solutions/2-B.cpp
@@ -0,0 +1,10 @@
+// The least round way
+// Contest: 2
+// Problem: B
+// Submission ID: 368237067
+// URL: https://codeforces.com/contest/2/submission/368237067
+
+// Language: C++23 (GCC 14-64, msys2)
+// Verdict: OK
+
+// TODO: Replace with actual solution code
